cba48757f7cd9af58028317497ffbcf40e1ef630
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2022-11-10 10:56:21 +0100

    feat: update ts-eslint naming convetion to allow leading underscore in var names

--- @r3wy.eslint-config/targets/common.js
+++ @r3wy.eslint-config/targets/common.js
@@ -13,6 +13,7 @@ module.exports = {
       "error",
       {
         selector: "default",
+        leadingUnderscore: "allow",
         format: ["camelCase", "PascalCase", "UPPER_CASE"],
       },
       {
