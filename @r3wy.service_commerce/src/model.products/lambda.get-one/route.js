cb0a9b61eaddbb3e9dacd4e13357bdd173bcb8a8
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2023-01-16 09:58:16 +0100

    chore: fix lint error

--- @r3wy.service_commerce/src/model.products/lambda.get-one/route.js
+++ @r3wy.service_commerce/src/model.products/lambda.get-one/route.js
@@ -12,10 +12,12 @@ export const getOne = async (event, context) => {
   console.log(`Event: ${JSON.stringify(event.pathParameters, null, 2)}`)
   console.log(`Context: ${JSON.stringify(context.functionName, null, 2)}`)
 
+  const { id } = event.pathParameters || {}
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      id: event.pathParameters.id,
+      id,
       message: "hello world",
     }),
   }
